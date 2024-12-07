9ee589f95e8154348163f6b1cf0bdea71b53b5c1
jg <jihonggan1@gmail.com>
2024-12-06 22:04:39 -0800

    day 7 part 2

--- 7/main.py
+++ 7/main.py
@@ -9,6 +9,17 @@ def combinable(nums, target):
         i += 1
     return target in results
 
+def combinable2(nums, target):
+    results = [nums[0]]
+    i = 1
+    while i < len(nums):
+        new_results = []
+        for n in results:
+            new_results.extend([n * nums[i], n + nums[i], int(str(n) + str(nums[i]))])
+        results = new_results
+        i += 1
+    return target in results
+
 def read_input(filename):
     records = []
     with open(filename, "r") as f:
@@ -21,11 +32,14 @@ def read_input(filename):
 
 def main():
     records = read_input("7/input.txt")
-    s = 0
+    s1, s2 = 0, 0
     for target, nums in records:
         if combinable(nums, target):
-            s += target
-    print("Part 1: ", s)
+            s1 += target
+        if combinable2(nums, target):
+            s2 += target
+    print("Part 1: ", s1)
+    print("Part 2: ", s2)
 
 if __name__ == "__main__":
     main()
